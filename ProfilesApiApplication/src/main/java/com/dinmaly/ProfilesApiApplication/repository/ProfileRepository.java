66b02f4766577580181df2f110b77ea9d8eb498b
officialsanchi <nwangwudinma22@gmail.com>
2026-04-16 15:01:42 +0100

    updated

--- ProfilesApiApplication/src/main/java/com/dinmaly/ProfilesApiApplication/repository/ProfileRepository.java
+++ ProfilesApiApplication/src/main/java/com/dinmaly/ProfilesApiApplication/repository/ProfileRepository.java
@@ -15,9 +15,9 @@ public interface ProfileRepository extends JpaRepository<Profile, String> {
     Optional<Profile> findByNameIgnoreCase(String name);
 
     @Query("SELECT p FROM Profile p WHERE " +
-            "(:gender IS NULL OR LOWER(CAST(p.gender AS text)) = LOWER(:gender)) AND " +
-            "(:countryId IS NULL OR LOWER(CAST(p.countryId AS text)) = LOWER(:countryId)) AND " +
-            "(:ageGroup IS NULL OR LOWER(CAST(p.ageGroup AS text)) = LOWER(:ageGroup))")
+            "(:gender IS NULL OR LOWER(p.gender) = LOWER(:gender)) AND " +
+            "(:countryId IS NULL OR LOWER(p.countryId) = LOWER(:countryId)) AND " +
+            "(:ageGroup IS NULL OR LOWER(p.ageGroup) = LOWER(:ageGroup))")
     List<Profile> findByFilters(
             @Param("gender") String gender,
             @Param("countryId") String countryId,
