3ac737855ccd0298bb3d732d7dbe8837539a1b29
officialsanchi <nwangwudinma22@gmail.com>
2026-04-16 15:12:56 +0100

    added a new query param

--- ProfilesApiApplication/src/main/java/com/dinmaly/ProfilesApiApplication/repository/ProfileRepository.java
+++ ProfilesApiApplication/src/main/java/com/dinmaly/ProfilesApiApplication/repository/ProfileRepository.java
@@ -14,13 +14,13 @@ public interface ProfileRepository extends JpaRepository<Profile, String> {
 
     Optional<Profile> findByNameIgnoreCase(String name);
 
-    @Query("SELECT p FROM Profile p WHERE " +
-            "(:gender IS NULL OR LOWER(p.gender) = LOWER(:gender)) AND " +
-            "(:countryId IS NULL OR LOWER(p.countryId) = LOWER(:countryId)) AND " +
-            "(:ageGroup IS NULL OR LOWER(p.ageGroup) = LOWER(:ageGroup))")
+    @Query(value = "SELECT * FROM profiles WHERE " +
+            "(:gender IS NULL OR LOWER(gender::text) = LOWER(:gender)) AND " +
+            "(:countryId IS NULL OR LOWER(country_id::text) = LOWER(:countryId)) AND " +
+            "(:ageGroup IS NULL OR LOWER(age_group::text) = LOWER(:ageGroup))",
+            nativeQuery = true)
     List<Profile> findByFilters(
             @Param("gender") String gender,
             @Param("countryId") String countryId,
-            @Param("ageGroup") String ageGroup
-    );
+            @Param("ageGroup") String ageGroup);
 }
