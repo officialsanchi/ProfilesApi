39bf0e8514589c675f615eb6d69faeb6c6d7a5bb
officialsanchi <nwangwudinma22@gmail.com>
2026-04-16 14:48:19 +0100

    updated the model varibles and annotattions, repository

--- ProfilesApiApplication/src/main/java/com/dinmaly/ProfilesApiApplication/model/Profile.java
+++ ProfilesApiApplication/src/main/java/com/dinmaly/ProfilesApiApplication/model/Profile.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.JdbcTypeCode;
 
+import java.sql.Types;
 import java.time.Instant;
 
 @Entity
